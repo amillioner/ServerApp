2c4ed2016a4b984e01ba653c086ab2dbd3069e4e
anwar akhror <anwar.akhror@outlook.com>
2020-03-05 14:38:08 -0500

    ServerApp

---
+++ Controllers/SupplierValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerApp.Models;
+using ServerApp.Models.BindingTargets;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ServerApp.Controllers
+{
+    [Route("api/suppliers")]
+    [Authorize(Roles = "Administrator")]
+    public class SupplierValuesController : Controller
+    {
+        //private DataContext context;
+        //// ...methods omitted for brevity...
+    }
+}
